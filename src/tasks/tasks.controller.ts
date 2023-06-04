70c39392eccb0ef3b6506d8af890ea0bb156b4e8
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-06-04 20:49:26 +0700

    add delete, update, and get tasks in controller and service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,21 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
+  Patch,
   Post,
+  Query,
   ValidationPipe,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto, TaskDto } from './dto/task.dto';
+import { DeleteResult } from 'typeorm';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { TaskStatus } from './enum/task.status.enum';
+import { GetTaskFilterDto } from './dto/get-task-fliter.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -24,5 +31,25 @@ export class TasksController {
     @Body(ValidationPipe) createTask: CreateTaskDto,
   ): Promise<TaskDto> {
     return this.tasksService.createTask(createTask);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  ): Promise<TaskDto> {
+    return this.tasksService.updateStatusTask(id, status);
+  }
+
+  @Get()
+  getTasks(
+    @Query(ValidationPipe) filter: GetTaskFilterDto,
+  ): Promise<TaskDto[]> {
+    return this.tasksService.getTask(filter);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,11 @@
 import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto, TaskDto } from './dto/task.dto';
 import { Task } from './task.entity';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TaskStatus } from './enum/task.status.enum';
 import { ConfigService } from '@nestjs/config';
+import { GetTaskFilterDto } from './dto/get-task-fliter.dto';
 
 @Injectable()
 export class TasksService {
@@ -39,5 +40,47 @@ export class TasksService {
     await task.save();
 
     return task;
+  }
+
+  async deleteTask(id: number): Promise<DeleteResult> {
+    const task = await this.taskRepository.delete(id);
+
+    if (task.affected === 0) {
+      throw new NotFoundException(`Task with "${id}" not found`);
+    }
+
+    return task;
+  }
+
+  async updateStatusTask(id: number, status: TaskStatus): Promise<TaskDto> {
+    const task = await this.getTaskById(id);
+    task.status = status;
+    await this.taskRepository.save(task);
+
+    return task;
+  }
+
+  async getTask(filter: GetTaskFilterDto): Promise<TaskDto[]> {
+    const query = this.taskRepository.createQueryBuilder('task');
+
+    if (filter.status) {
+      query.andWhere('task.status = :status', { status: filter.status });
+    }
+
+    if (filter.search) {
+      query.andWhere(
+        '(task.title LIKE :search OR task.description LIKE :search)',
+        { search: `%${filter.search}%` },
+      );
+    }
+
+    const tasks = await query.getMany();
+
+    return tasks.map((task) => ({
+      id: task.id,
+      description: task.description,
+      status: task.status,
+      title: task.title,
+    }));
   }
 }
